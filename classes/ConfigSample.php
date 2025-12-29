4d8dea583c1de0a17c2838e44fb1ed6ca3b6ac3e
Rob Nugen <mg.robnugen.com@robnugen.com>
2025-12-28 22:05:49 -0800

    Clean up ConfigSample

--- classes/ConfigSample.php
+++ classes/ConfigSample.php
@@ -2,12 +2,12 @@
 
 class Config {
 
-    public $domain_name = 'db.marbletrack3.com';  // used for cookies
-    public $cookie_name = 'mt3login'; // used for cookies
+    public $domain_name = '';  // used for cookies
+    public $cookie_name = '';  // used for cookies
     public $cookie_lifetime = 60 * 60 * 24 * 30; // 30 days
-    public $app_path = '/home/barefoot_rob/db.marbletrack3.com';
+    public $app_path = '/home/user/sub.domain.com';
 
-    public $dbHost = "localhost";
+    public $dbHost = "eich";
     public $dbUser = "";
     public $dbPass = "";
     public $dbName = "";
